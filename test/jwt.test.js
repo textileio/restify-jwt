0d054b473bceba73c1aa1c8953857c14fb522854
Vikrant Varma <vikrant.varma94@gmail.com>
2015-10-08 05:14:11 +0530

    Release v0.3.0

--- test/jwt.test.js
+++ test/jwt.test.js
@@ -242,19 +242,19 @@ describe('work tests', function () {
       assert.equal('bar', req.user.foo);
     });
   });
-  
+
   it('should work with a secretCallback function that accepts header argument', function() {
     var secret = 'shhhhhh';
     var secretCallback = function(req, headers, payload, cb) {
       assert.equal(headers.alg, 'HS256');
       assert.equal(payload.foo, 'bar');
-      process.nextTick(function(){ return cb(null, secret) });
-    }
+      process.nextTick(function(){ return cb(null, secret); });
+    };
     var token = jwt.sign({foo: 'bar'}, secret);
 
     req.headers = {};
     req.headers.authorization = 'Bearer ' + token;
-    expressjwt({secret: secretCallback})(req, res, function() {
+    restifyjwt({secret: secretCallback})(req, res, function() {
       assert.equal('bar', req.user.foo);
     });
   });
